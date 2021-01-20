290774e51cabf0534f522307e1502b3627850304
Mintakai <60737572+Mintakai@users.noreply.github.com>
2021-01-21 01:26:55 +0200

    updates

--- regex_training_continued_3.py
+++ regex_training_continued_3.py
@@ -4,6 +4,6 @@ phone_number = "+358 50-123-456789"
 
 print(re.match(r"^\+?\d{1,20}$", re.sub(r"[ -]", "", phone_number)))
 
-result = str(re.match(r"^\+?\d{1,20}$", re.sub(r"[ -]", "", phone_number)))
-result = re.search('\+\d+', result)
-print(result.group(0))
+result = str(re.match(r"^\+?\d{1,20}$", re.sub(r"[ -]", "", phone_number))) # Mysteeririvit
+result = re.search('\+\d+', result)                                         # Mysteeririvit
+print(result.group(0))                                                      # Mysteeririvit
