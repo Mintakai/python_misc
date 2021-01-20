bc093153172541a68de2e20e26efd9e678e64b47
Mintakai <60737572+Mintakai@users.noreply.github.com>
2021-01-21 01:25:40 +0200

    Update regex_training_continued_3.py

--- regex_training_continued_3.py
+++ regex_training_continued_3.py
@@ -4,6 +4,6 @@ phone_number = "+358 50-123-456789"
 
 print(re.match(r"^\+?\d{1,20}$", re.sub(r"[ -]", "", phone_number)))
 
-result = str(re.match(r"^\+?\d{1,20}$", re.sub(r"[ -]", "", phone_number)))     # Don't mind me...
-result = re.search('\+\d+', result)                                             # Just doing something...
-print(result.group(0))                                                          # I will never understand this shit
+result = str(re.match(r"^\+?\d{1,20}$", re.sub(r"[ -]", "", phone_number)))
+result = re.search('\+\d+', result)
+print(result.group(0))
