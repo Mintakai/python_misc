fabfa329b01742a67add7faf54c663aea7058e33
Mintakai <toni19129@student.hamk.fi>
2021-01-25 10:48:25 +0200

    updates

--- filemanip.py
+++ filemanip.py
@@ -3,19 +3,11 @@
 # Jos kaikki menee oikein, ohjelma tulostaa vastauksen "Saatiin tulos [tulos]". Jos annettu tiedoston nimi on virheellinen, tulostetaan "Virheellinen tiedostonnimi". 
 # Jos taas tiedoston sisältö on virheellinen eikä käänny kokonaisluvuksi, tulostetaan "Tiedoston sisältö virheellinen!". Toimiessaan ohjelma tulostaakin seuraavaa:
 
-filen = input("Anna tiedoston nimi: ")
-
 try:
-    with open(filen, 'r') as f:
+    with open(input("Anna tiedoston nimi: "), 'r') as f:
         content = f.read()
+    print(f"Saatiin tulos {int(content) + 313}")
 except IOError:
-    print("Virheellinen tiedostonimi")
-    quit()
-
-try:
-    content = int(content)
+    print("Virheellinen tiedoston nimi")
 except ValueError:
     print("Tiedoston sisältö virheellinen!")
-content += 313
-
-print(f"Saatiin tulos: {content}")
